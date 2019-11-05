f5bed4b0b4127478422fd13434a456863801b37e
RunheZH <runhe.zhong@gmail.com>
2019-11-05 14:48:43 -0500

    program.save

--- entity/inc/program.h
+++ entity/inc/program.h
@@ -4,11 +4,25 @@
 #include "common_lib.h"
 #include "identifier.h"
 #include "statement.h"
+#include "../stmts/inc/addStmt.h"
+#include "../stmts/inc/compStmt.h"
+#include "../stmts/inc/declArrStmt.h"
+#include "../stmts/inc/declIntStmt.h"
+#include "../stmts/inc/endStmt.h"
+#include "../stmts/inc/jEqStmt.h"
+#include "../stmts/inc/jLessStmt.h"
+#include "../stmts/inc/jMoreStmt.h"
+#include "../stmts/inc/jumpStmt.h"
+#include "../stmts/inc/movStmt.h"
+#include "../stmts/inc/printStmt.h"
+#include "../stmts/inc/readStmt.h"
+#include <QFile>
+#include <QRegExp>
 
 class Program
 {
 public:
-    Program(QString pgmName, QString pgmPath);
+    Program(QString, QString);
 	~Program();
 	ResultState save();
 	ResultState compile();
@@ -18,8 +32,13 @@ public:
 private:
     QString pgmName;
     QString pgmPath;
+    qint16  numStmt;
 	Identifier** ids;
 	Statement**  statements;
+
+    // helper functions
+    ResultState addStmt(QString);
+    StatementId getStmtId(QString);
 	//std::map<StatementId, Statement*> m_statementMap;
 };
 
--- entity/src/program.cpp
+++ entity/src/program.cpp
@@ -1,56 +1,134 @@
 #include "../inc/program.h"
 
+/*********************************** API ***************************************/
+
 Program::Program(QString pgmName, QString pgmPath)
 {
     this->pgmName = pgmName;
     this->pgmPath = pgmPath;
-	/*
-	m_statementMap.insert(std::make_pair(StatementId::READ_STMT, Read::getInstance()));
-	m_statementMap.insert(std::make_pair(StatementId::PRINT_STMT, Print::getInstance()));
-	m_statementMap.insert(std::make_pair(StatementId::DECLINT_STMT, DeclInt::getInstance()));
-	m_statementMap.insert(std::make_pair(StatementId::DECLARR_STMT, DeclArr::getInstance()));
-	m_statementMap.insert(std::make_pair(StatementId::MOV_STMT, Mov::getInstance()));
-	m_statementMap.insert(std::make_pair(StatementId::ADD_STMT, Add::getInstance()));
-	m_statementMap.insert(std::make_pair(StatementId::COMP_STMT, Comp::getInstance()));
-	m_statementMap.insert(std::make_pair(StatementId::JLESS_STMT, JLess::getInstance()));
-	m_statementMap.insert(std::make_pair(StatementId::JMORE_STMT, JMore::getInstance()));
-	m_statementMap.insert(std::make_pair(StatementId::JEQ_STMT, JEq::getInstance()));
-	m_statementMap.insert(std::make_pair(StatementId::JUMP_STMT, Jump::getInstance()));
-	m_statementMap.insert(std::make_pair(StatementId::END_STMT, End::getInstance()));
-	*/
+    this->numStmt = 0;
+}
+
+Program::~Program()
+{
+    for (qint16 i=0; i<this->numStmt; i++)
+    {
+        delete(this->statements[i]);
+    }
 }
 
 ResultState Program::save()
 {
+    qDebug() << "RUNHE: Program::save()";
+    QFile file(this->pgmPath + this->pgmName + ".scp");
+
+    if (!file.open(QIODevice::ReadOnly | QIODevice::Text))
+        return FILE_OPEN_ERROR;
+
+    while (!file.atEnd()) {
+        QString line = file.readLine();
+        ResultState res = addStmt(line);
+        // TODO: error recovery
+        if (res != NO_ERROR)
+            qDebug() << res << "at line" << this->numStmt;
+    }
+
+    file.close();
+    return NO_ERROR;
 }
 
 ResultState Program::compile()
 {
-    qDebug() << "Hello there";
-	/*
-	ResultState aResultState;
-	for(;;){
-		aStatementId = ;
-		aResultState = compileSingle(aStatementId, aStatement);
-		if (aResultState != ResultState::NO_ERROR)
-			return aResultState;		
-	}
-	return aResultState;
-	*/
+    qDebug() << "RUNHE: Program::compile()";
+    // TODO: calling compile()
+    // TODO: error recovery
+    return NO_ERROR;
 }
 
 ResultState Program::run()
 {
+    qDebug() << "RUNHE: Program::run()";
+    // TODO
+    return NO_ERROR;
 }
 
-/*
-ResultState Program::compileSingle(StatementId stmtID, QString aStmt)
+/*********************************** private helper functions ***************************************/
+
+ResultState Program::addStmt(QString stmt)
 {
-    auto it = find_if (programDatas.begin (), programDatas.end (),
-        [aStatementId] (const std::map<StatementId, Statement*> &element) { return element.first == aStatementId;});
-    if (it != programDatas.end ()) {
-        return (*it).second->complieStatement;
-	}
- 
-	return 0;
-}*/
+    QStringList args = stmt.split(QRegExp("\\s+"), QString::SkipEmptyParts);
+    Statement* newStmt;
+    // TODO: check label
+    switch (this->getStmtId(args[0])) {
+    case ADD_STMT:
+        newStmt = new AddStmt(this->pgmName, stmt, nullptr);
+        break;
+    case CMP_STMT:
+        newStmt = new CompStmt(this->pgmName, stmt, nullptr);
+        break;
+    case DCA_STMT:
+        newStmt = new DeclArrStmt(this->pgmName, stmt, nullptr);
+        break;
+    case DCI_STMT:
+        newStmt = new DeclIntStmt(this->pgmName, stmt, nullptr);
+        break;
+    case END_STMT:
+        newStmt = new EndStmt(this->pgmName, stmt, nullptr);
+        break;
+    case JEQ_STMT:
+        newStmt = new JEqStmt(this->pgmName, stmt, nullptr);
+        break;
+    case JLS_STMT:
+        newStmt = new JLessStmt(this->pgmName, stmt, nullptr);
+        break;
+    case JMR_STMT:
+        newStmt = new JMoreStmt(this->pgmName, stmt, nullptr);
+        break;
+    case JMP_STMT:
+        newStmt = new JumpStmt(this->pgmName, stmt, nullptr);
+        break;
+    case MOV_STMT:
+        newStmt = new MovStmt(this->pgmName, stmt, nullptr);
+        break;
+    case PRT_STMT:
+        newStmt = new PrintStmt(this->pgmName, stmt, nullptr);
+        break;
+    case RDI_STMT:
+        newStmt = new ReadStmt(this->pgmName, stmt, nullptr);
+        break;
+    case INVALID_STMT:
+        return INVALID_STATEMENT;
+    }
+
+    this->statements[this->numStmt] = newStmt;
+    this->numStmt++;
+    return NO_ERROR;
+}
+
+StatementId Program::getStmtId(QString stmt)
+{
+    if(stmt == "add")
+        return ADD_STMT;
+    else if(stmt == "cmp")
+        return CMP_STMT;
+    else if(stmt == "dca")
+        return DCA_STMT;
+    else if(stmt == "dci")
+        return DCI_STMT;
+    else if(stmt == "end")
+        return END_STMT;
+    else if(stmt == "jeq")
+        return JEQ_STMT;
+    else if(stmt == "jls")
+        return JLS_STMT;
+    else if(stmt == "jmr")
+        return JMR_STMT;
+    else if(stmt == "mov")
+        return MOV_STMT;
+    else if(stmt == "prt")
+        return PRT_STMT;
+    else if(stmt == "rdi")
+        return RDI_STMT;
+    else
+        return INVALID_STMT;
+}
